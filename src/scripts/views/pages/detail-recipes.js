0a2ee0f78c0ab84aa300795cea6e3dcb01be3ad9
fadiains <indahsarifadia@gmail.com>
2023-12-07 14:03:20 +0700

    nambahin css detail-resep

--- src/scripts/views/pages/detail-recipes.js
+++ src/scripts/views/pages/detail-recipes.js
@@ -1,11 +1,10 @@
-import UrlParser from '../../routes/url-parser';
-import { createRecipesDetailTemplate } from '../templates/template-creator';
-import TheRecipesSource from '../../data/therecipes-source';
+import UrlParser from "../../routes/url-parser";
+import { createRecipesDetailTemplate } from "../templates/template-creator";
+import TheRecipesSource from "../../data/therecipes-source";
 
 const DetailRecipes = {
   async render() {
     return `
-   <h2 class="title" >Detail Recipes Page</h2>
    <div id="result" class="result">
    <div id="likeButtonContainer"></div>
     
@@ -16,12 +15,12 @@ const DetailRecipes = {
     // Fungsi ini akan dipanggil setelah render()
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const detail = await TheRecipesSource.DetailRecipe(url.name);
-    const recipesContainer = document.querySelector('#result');
+    const recipesContainer = document.querySelector("#result");
     detail.forEach((result) => {
       recipesContainer.innerHTML = createRecipesDetailTemplate(result);
     });
     LikeButtonInitiator.init({
-      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
       result: {
         id: result.id,
         name: result.name,
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,10 +1,8 @@
-import CONFIG from '../../globals/config';
+import CONFIG from "../../globals/config";
 
 const createRecipesDetailTemplate = (result) => `
 <h2 class="result-name">${result.name}</h2>
-  <img class="result-picture" src="${CONFIG.BASE_IMAGE_URL + result.id}" alt="${
-  result.name
-}"/>
+  <img class="result-picture" src="${CONFIG.BASE_IMAGE_URL + result.id}" alt="${result.name}"/>
   <div class="result-description">
     <h3>Deskripsi</h3>
     <p>${result.desc}</p>
@@ -12,16 +10,11 @@ const createRecipesDetailTemplate = (result) => `
   <div class="result-info">
   <h3>Bahan-bahan</h3>
   <ul class="ingredients-list">
-  ${result.ingredients
-    .map(
-      (ingredient) =>
-        ` <li>${ingredient.bahan}</li><li>${ingredient.jumlah}</li>`
-    )
-    .join('•')}
+  ${result.ingredients.map((ingredient) => ` <li>${ingredient.bahan}: ${ingredient.jumlah}</li>`).join("")}
 </ul>
   <h3>Langkah-langkah</h3>
     <ol class="result-steps">
-      ${result.receipt.map((receipts) => `<li>${receipts}</li>`).join('•')}
+      ${result.receipt.map((receipts) => `<li>${receipts}</li>`).join("")}
     </ol>
   </div>
  
@@ -32,16 +25,12 @@ const createRecipeItemTemplate = (result) => `
          <div class="overlay">
            <div class="image-content">
              <div class="card-image">
-               <img src="${CONFIG.BASE_IMAGE_URL + result.id}" alt="${
-  result.name || '-'
-}"class="card-img" />
+               <img src="${CONFIG.BASE_IMAGE_URL + result.id}" alt="${result.name || "-"}"class="card-img" />
              </div>
            </div>
          </div>
          <div class="card-content">
-           <h2 class="name"><a href="${`/#/detail-recipes/${result.name}`}">${
-  result.name || '-'
-}</a></h2>
+           <h2 class="name"><a href="${`/#/detail-recipes/${result.name}`}">${result.name || "-"}</a></h2>
            <p class="description">${result.desc}</p>
 
            <button class="button"><a href="${`/#/detail-recipes/${result.name}`}">View More</a></button>
@@ -56,16 +45,12 @@ const createRegionItemTemplate = (result) => `
          <div class="overlay">
            <div class="image-content">
              <div class="card-image">
-               <img src="${CONFIG.BASE_IMAGE_URL + result.id}" alt="${
-  result.name || '-'
-}"class="card-img" />
+               <img src="${CONFIG.BASE_IMAGE_URL + result.id}" alt="${result.name || "-"}"class="card-img" />
              </div>
            </div>
          </div>
          <div class="card-content">
-           <h2 class="name"><a href="${`/#/detail-regions/${result}`}">${
-  result || '-'
-}</a></h2>
+           <h2 class="name"><a href="${`/#/detail-regions/${result}`}">${result || "-"}</a></h2>
            <p class="description">${result.desc}</p>
 
            <button class="button"><a href="${`/#/detail-recipes/${result.regional}`}">View More</a></button>
@@ -86,10 +71,4 @@ const createLikedButtonTemplate = () => `
   </button>
 `;
 
-export {
-  createRecipeItemTemplate,
-  createRecipesDetailTemplate,
-  createRegionItemTemplate,
-  createLikeButtonTemplate,
-  createLikedButtonTemplate,
-};
+export { createRecipeItemTemplate, createRecipesDetailTemplate, createRegionItemTemplate, createLikeButtonTemplate, createLikedButtonTemplate };
